7736b8e4fe8934a96495f61807c20a089e88f5ec
Vanessa Pacheco <pacheco.e.vanessa@gmail.com>
2019-03-26 23:08:49 -0700

    Add design and update features

--- main.js
+++ main.js
@@ -1,44 +1,73 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  var $data_paragraph = $("#data-paragraph");
+  var $passage = $("#passage");
+  var $title = $("#title");
 
-
-    document.addEventListener(
-        "mouseup",
-        function() {
-          var highlightedText = getSelectionText();
-          if (highlightedText.length > 0) {
-            // check there's some text selected
-            word = highlightedText;
-            dictionarySearch();
-            console.log(highlightedText); // logs whatever textual content the user has selected on the page
-          }
-        },
-        false
-      );
-
-      function getSelectionText() {
-        var selectedText = "";
-        if (window.getSelection) {
-          // all modern browsers and IE9+
-          selectedText = window.getSelection().toString();
-        }
-        return selectedText;
+  document.addEventListener(
+    "mouseup",
+    function() {
+      var highlightedText = getSelectionText();
+      if (highlightedText.length > 0) {
+        // check there's some text selected
+        word = highlightedText.toLowerCase();
+        dictionarySearch();
+        console.log(highlightedText); // logs whatever textual content the user has selected on the page
       }
+    },
+    false
+  );
 
-      function dictionarySearch() {
-          $("#data_paragraph").html("");
-          let url = "https://cors-anywhere.herokuapp.com/https://jisho.org/api/v1/search/words/?keyword=" + word;
-          $.ajax({
-            type: 'GET',
-            url: url,
-            success: function(res) {
-                $.each(res.data, function(i, item) {
-                    $("#data_paragraph").append("<li>" + JSON.stringify(item.senses[0].english_definitions) + " " + JSON.stringify(item.japanese[0].word) + " " + JSON.stringify(item.japanese[0].reading) + "</li>");
-                })
-    
-            }
-        })
-        }
-      
+  $("#save-passage").on("click", function() {
+    var item = {
+      title: $title.val(),
+      passage: $passage.val()
+    };
 
+    $.ajax({
+      type: 'POST',
+      url: './api/saved_passages',
+      data: item,
+      success: function(newItem) {
+        alert('passage saved');
+      },
+      error: function() {
+        alert('error saving passage')
+      }
+    });
+  });
+
+  function getSelectionText() {
+    var selectedText = "";
+    if (window.getSelection) {
+      // all modern browsers and IE9+
+      selectedText = window.getSelection().toString();
+    }
+    return selectedText;
+  }
+
+  function dictionarySearch() {
+    $data_paragraph.html("");
+    let url =
+      "https://cors-anywhere.herokuapp.com/https://jisho.org/api/v1/search/words/?keyword=" +
+      word;
+    console.log(url);
+    $.ajax({
+      type: "GET",
+      url: url,
+      success: function(res) {
+        console.log(res);
+        $.each(res.data, function(i, item) {
+          $data_paragraph.append(
+            "<li id='dictionary-item' class='dictionary-item'>" +
+              JSON.stringify(item.senses[0].english_definitions) +
+              " " +
+              JSON.stringify(item.japanese[0].word) +
+              " " +
+              JSON.stringify(item.japanese[0].reading) +
+              "</li>"
+          );
+        });
+      }
+    });
+  }
 });
-
